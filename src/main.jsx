cf5d48b28e60fb3b9000adbf12c74ba808d3f3a5
Andrii Hrytsei <andrii.hrytsei@gmail.com>
2023-07-31 18:27:58 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-navigation">
+    <BrowserRouter basename="/react-navigation/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
